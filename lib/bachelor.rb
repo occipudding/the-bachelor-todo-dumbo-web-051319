840fde0488fbcd00be7f101dc9e23252b944db8c
Joshua Suskin <jsuskin85@gmail.com>
2019-05-09 14:12:00 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,59 @@
+require 'pry'
+
+#=begin
+two_seasons = { 
+  "season 30": [
+    {
+      "name":      "Beth Smalls",
+      "age":       "26",
+      "hometown":  "Great Falls, Virginia",
+      "occupation":"Nanny/Freelance Journalist",
+      "status":    "Winner"
+    },
+    {
+      "name":       "Becca Tilley",
+      "age":        "27",
+      "hometown":   "Shreveport, Louisiana",
+      "occupation": "Chiropractic Assistant",
+      "status":     "Eliminated Week 8"
+    }
+  ],
+  "season 29": [
+    {
+      "name":      "Ashley Yeats",
+      "age":       "24",
+      "hometown":  "Denver, Colorado",
+      "occupation":"Dental Assitant",
+      "status":    "Winner"
+    },
+    {
+      "name":       "Sam Grover",
+      "age":        "29",
+      "hometown":   "New York, New York",
+      "occupation": "Entertainer",
+      "status":     "Eliminated Week 6"
+    }
+  ]
+}
+#=end
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  arr = data[season]
+  arr.each do |obj|
+    if obj["status"] == "Winner"
+      return obj["name"].split(" ")[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |k, v|
+    v.each do |obj|
+      if obj[:occupation] == occupation
+        return obj[:name]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
@@ -17,3 +67,5 @@ end
 def get_average_age_for_season(data, season)
   # code here
 end
+
+get_contestant_name(two_seasons, "Entertainer")
