05b41fa45d8be237bdcfdaba49e34631b536b31a
Joshua Suskin <jsuskin85@gmail.com>
2019-05-09 14:43:09 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,23 +49,45 @@ end
 def get_contestant_name(data, occupation)
   data.each do |k, v|
     v.each do |obj|
-      if obj[:occupation] == occupation
-        return obj[:name]
+      if obj["occupation"] == occupation
+        return obj["name"]
       end
     end
   end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |k, v|
+    v.each do |obj|
+      if obj["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |k, v|
+    v.each do |obj|
+      if obj["hometown"] == hometown
+        return obj["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  total = 0
+  data[season].each do |obj|
+    ages << obj["age"]
+  end
+  ages.each do |age|
+    total += age.to_f
+  end
+  (total / ages.size).round
 end
 
 #get_contestant_name(two_seasons, "Entertainer")
